f328f8964aab6b27a31aad3c3f4f874aae8ab616
Vitaly Panev <panevvitalij@gmail.com>
2023-10-17 18:49:41 +0300

    added number parity check

--- EvenAndNotEven.cpp
+++ EvenAndNotEven.cpp
@@ -3,5 +3,19 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   static const int EvenNumber = 24;
+
+	for (int i = 1; i <= EvenNumber; i++)
+	{
+		if (i % 2 == 0)
+		{
+			std::cout << i << "\n";
+		}
+	}
+	return 0;
 }
+
+//int OddNumber()
+//{
+//
+//}
