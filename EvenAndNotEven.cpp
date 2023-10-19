8d496a2cb59e13425540398674f620fd0a34d464
Vitaly Panev <panevvitalij@gmail.com>
2023-10-19 20:37:51 +0300

    output of even numbers and odd

--- EvenAndNotEven.cpp
+++ EvenAndNotEven.cpp
@@ -1,25 +1,27 @@
 ﻿
 #include <iostream>
 
+const int EvenNumber = 15;
+
 void NotEven(int EvenNumber)
 {
-	EvenNumber = 15;
-	for (int j = 0; j <= EvenNumber; j++)
+	
+	for (int j = 1; j <= EvenNumber; j++)
 	{
+		
 		if (j % 2 != 0)
 		{
 			std::cout << j << " Odd number" << "\n";
+		}
+		else
+		{
+			std::cout << j << " Even Number" << "\n";
 		}
 	}
 }
 
 int main()
 {
-	static const int EvenNumber = 24;
-	for (int i = 2; i <= EvenNumber; i += 2)
-	{
-			std::cout << i << " Even Number" << "\n";
-	}
 	NotEven(EvenNumber);
 	return 0;
 }
